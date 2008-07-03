3ba3eb861cb4f0bd57cacfa5401d29b78a7a0e8e
badlop <badlop@d5c1f840-3623-0410-9cd6-eede35f65a94>
2008-07-03 09:45:23 +0000

    Fixed code indentation with Emacs erlang-mode. Update INFO_MSG.

--- src/mod_rest.erl
+++ src/mod_rest.erl
@@ -1,9 +1,9 @@
-%%%
+%%%-------------------------------------------------------------------
 %%% File    : mod_rest.erl
 %%% Author  : Nolan Eakins <sneakin@semanticgap.com>
-%%% Purpose : Provide an HTTP interface to POST stanzas into Ejabberd.
+%%% Purpose : Provide an HTTP interface to POST stanzas into ejabberd
 %%%
-%%% Copyright (C) 2008 Nolan Eakins. 
+%%% Copyright (C) 2008 Nolan Eakins
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
@@ -20,17 +20,17 @@
 %%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 %%% 02111-1307 USA
 %%%
+%%%-------------------------------------------------------------------
 
 -module(mod_rest).
 -author('sneakin@semanticgap.com').
--vsn('0.1').
 
 -behavior(gen_mod).
 
--export([ start/2,
-	  stop/1,
-	  process/2
-	 ]).
+-export([start/2,
+	 stop/1,
+	 process/2
+	]).
 
 -include("ejabberd.hrl").
 -include("jlib.hrl").
@@ -41,7 +41,7 @@ start(_Host, _Opts) ->
     RESTSupervisor = {
       ejabberd_mod_rest_sup, {
 	ejabberd_tmp_sup, start_link,
-	[ ejabberd_mod_rest_sup, ejabberd_mod_rest ]
+	[ejabberd_mod_rest_sup, ejabberd_mod_rest]
        },
       permanent,
       infinity,
@@ -49,38 +49,40 @@ start(_Host, _Opts) ->
       [ejabberd_tmp_sup]
      },
     case supervisor:start_child(ejabberd_sup, RESTSupervisor) of
-	{ok, _Pid } -> ok;
+	{ok, _Pid} -> ok;
 	{ok, _Pid, _Info} -> ok;
-	{error, Error } -> { 'EXIT', {start_child_error, Error}}
+	{error, Error } -> {'EXIT', {start_child_error, Error}}
     end.
 
 stop(_Host) ->
     case supervisor:terminate_child(ejabberd_sup, ejabberd_mod_rest_sup) of
 	ok -> ok;
-	{ error, Error } ->
-	    { 'EXIT', { terminate_child_error, Error }}
+	{error, Error} ->
+	    {'EXIT', {terminate_child_error, Error}}
     end.
 
 process([], #request{method = 'POST',
 		     data = Data,
-		     ip = _Ip
+		     ip = Ip
 		    }) ->
     Stanza = xml_stream:parse_element(Data),
     From = jlib:string_to_jid(xml:get_tag_attr_s("from", Stanza)),
     To = jlib:string_to_jid(xml:get_tag_attr_s("to", Stanza)),
-    ?INFO_MSG("Got request to ~p ~p ~p: ~p", [jlib:jid_to_string(From),
-					      jlib:jid_to_string(To),
-					      _Ip, Stanza]),
+    ?INFO_MSG("Got request from ~s with IP ~p to ~s:~n~p",
+	      [jlib:jid_to_string(From),
+	       Ip,
+	       jlib:jid_to_string(To),
+	       Stanza]),
     try
-      { xmlelement, "message", _Attrs, _Kids } = Stanza,
-      case ejabberd_router:route(From, To, Stanza) of
-	  ok -> { 200, [], "Ok"};
-	  _ -> { 500, [], "Error" }
-      end
+	{xmlelement, "message", _Attrs, _Kids} = Stanza,
+	case ejabberd_router:route(From, To, Stanza) of
+	    ok -> {200, [], "Ok"};
+	    _ -> {500, [], "Error"}
+	end
     catch
-	error:{ badmatch, _ } -> { 406, [], "Error: can only accept <message/>" };
-	error:{ Reason, _ } -> { 500, [], "Error: " ++ atom_to_list(Reason) }
+	error:{badmatch, _} -> {406, [], "Error: can only accept <message/>"};
+	  error:{Reason, _} -> {500, [], "Error: " ++ atom_to_list(Reason)}
     end;
 process(_Path, _Request) ->
     ?DEBUG("Got request to ~p: ~p", [_Path, _Request]),
-    { 200, [], "Try POSTing a stanza." }.
+    {200, [], "Try POSTing a stanza."}.
