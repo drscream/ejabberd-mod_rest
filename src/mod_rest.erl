48b838419f49eb1b0450363b6859a27e83b9f5d7
badlop <badlop@d5c1f840-3623-0410-9cd6-eede35f65a94>
2009-04-17 14:06:18 +0000

    Added support to execute ejabberd commands. Added AccessCommands option (EJAB-910).

--- src/mod_rest.erl
+++ src/mod_rest.erl
@@ -35,49 +35,41 @@
 -include("ejabberd.hrl").
 -include("jlib.hrl").
 -include("ejabberd_http.hrl").
+-include("ejabberd_ctl.hrl").
 
 start(_Host, _Opts) ->
     ?DEBUG("Starting: ~p ~p", [_Host, _Opts]),
-    RESTSupervisor = {
-      ejabberd_mod_rest_sup, {
-	ejabberd_tmp_sup, start_link,
-	[ejabberd_mod_rest_sup, ejabberd_mod_rest]
-       },
-      permanent,
-      infinity,
-      supervisor,
-      [ejabberd_tmp_sup]
-     },
-    case supervisor:start_child(ejabberd_sup, RESTSupervisor) of
-	{ok, _Pid} -> ok;
-	{ok, _Pid, _Info} -> ok;
-	{error, Error } -> {'EXIT', {start_child_error, Error}}
-    end.
+    ok.
 
 stop(_Host) ->
-    case supervisor:terminate_child(ejabberd_sup, ejabberd_mod_rest_sup) of
-	ok -> ok;
-	{error, Error} ->
-	    {'EXIT', {terminate_child_error, Error}}
-    end.
+    ok.
 
 process([], #request{method = 'POST',
 		     data = Data,
 		     host = Host,
 		     ip = ClientIp
 		    }) ->
-    maybe_post_request(Data, Host, ClientIp);
-process(_Path, _Request) ->
-    ?DEBUG("Got request to ~p: ~p", [_Path, _Request]),
+    try
+	{ClientAddress, _PortNumber} = ClientIp,
+	check_member_option(Host, ClientAddress, allowed_ips),
+	maybe_post_request(Data, Host, ClientIp)
+    catch
+	error:{badmatch, _} ->
+	    {406, [], "Error: REST request is rejected by service."}
+    end;
+process(Path, Request) ->
+    ?DEBUG("Got request to ~p: ~p", [Path, Request]),
     {200, [], "Try POSTing a stanza."}.
 
 
-maybe_post_request(Data, Host, ClientIp) ->
+%% If the first character of Data is <, it is considered a stanza to deliver.
+%% Otherwise, it is considered an ejabberd command to execute.
+maybe_post_request([$< | _ ] = Data, Host, ClientIp) ->
     try
 	Stanza = xml_stream:parse_element(Data),
         From = jlib:string_to_jid(xml:get_tag_attr_s("from", Stanza)),
         To = jlib:string_to_jid(xml:get_tag_attr_s("to", Stanza)),
-	allowed = check_stanza(Stanza, From, To, Host, ClientIp),
+	allowed = check_stanza(Stanza, From, To, Host),
 	?INFO_MSG("Got valid request from ~s~nwith IP ~p~nto ~s:~n~p",
 		  [jlib:jid_to_string(From),
 		   ClientIp,
@@ -91,11 +83,21 @@ maybe_post_request(Data, Host, ClientIp) ->
 	    {500, [], "Error: " ++ atom_to_list(Reason)};
 	_ ->
 	    {500, [], "Error"}
+    end;
+maybe_post_request(Data, Host, _ClientIp) ->
+    ?INFO_MSG("Data: ~p", [Data]),
+    Args = split_line(Data),
+    AccessCommands = gen_mod:get_module_opt(Host, ?MODULE, access_commands, []),
+    case ejabberd_ctl:process2(Args, AccessCommands) of
+	{String, ?STATUS_SUCCESS} ->
+	    {200, [], String};
+	{String, _Code} ->
+	    {500, [], String}
     end.
 
+
 %% This function crashes if the stanza does not satisfy configured restrictions
-check_stanza(Stanza, _From, To, Host, {ClientAddress, _PortNumber}) ->
-    check_member_option(Host, ClientAddress, allowed_ips),
+check_stanza(Stanza, _From, To, Host) ->
     check_member_option(Host, jlib:jid_to_string(To), allowed_destinations),
     {xmlelement, StanzaType, _Attrs, _Kids} = Stanza,
     check_member_option(Host, StanzaType, allowed_stanza_types),
@@ -112,3 +114,28 @@ post_request(Stanza, From, To) ->
 	ok -> {200, [], "Ok"};
         _ -> {500, [], "Error"}
     end.
+
+%% Split a line into args. Args are splitted by blankspaces. Args can be enclosed in "".
+%%
+%% Example call:
+%% mod_rest:split_line("  a1 b2 \"c3 d4\"e5\" c6   d7 \\\"  e8\"f9   g0 \\\" h1  ").
+%% ["a1","b2","c3 d4\"e5","c6","d7","  e8\"f9   g0 ","h1"]
+%%
+%% 32 is the integer that represents the blankspace
+%% 34 is the integer that represents the double quotes: "
+%% 92 is the integer that represents the backslash: \
+split_line(Line) -> split(Line, "", []).
+split("", "", Args) -> lists:reverse(Args);
+split("", Arg, Args) -> split("", "", [lists:reverse(Arg) | Args]);
+split([32 | Line], "", Args) -> split(Line, [], Args);
+split([32 | Line], Arg, Args) -> split(Line, [], [lists:reverse(Arg) | Args]);
+split([34 | Line], "", Args) -> {Line2, Arg2} = splitend(Line), split([32 | Line2], Arg2, Args);
+split([92, 34 | Line], "", Args) -> {Line2, Arg2} = splitend(Line), split([32 | Line2], Arg2, Args);
+split([Char | Line], Arg, Args) -> split(Line, [Char | Arg], Args).
+splitend(Line) -> splitend(Line, []).
+splitend([], Res) -> {"", Res};
+splitend([34], Res) -> {"", Res};
+splitend([92, 34], Res) -> {"", Res};
+splitend([34, 32 | Line], Res) -> {Line, Res};
+splitend([92, 34, 32 | Line], Res) -> {Line, Res};
+splitend([Char | Line], Res) -> splitend(Line, [Char | Res]).
